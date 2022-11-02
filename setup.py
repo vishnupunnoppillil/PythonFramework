710716006f86fa42e75dc246d2d8644f23cbad1a
vishnupunnoppillil <vish.fed@gmail.com>
2022-11-02 15:22:17 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PythonFramework',
+    version='',
+    packages=['Pages', 'TestCases', 'Utilities'],
+    url='',
+    license='',
+    author='vishnu.vijayan',
+    author_email='',
+    description=''
+)
